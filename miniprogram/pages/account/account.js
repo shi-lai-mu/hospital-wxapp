710f129fdb61eef40b34a05def0c3214ad71311e
ShiLaiMu <478889187@qq.com>
2018-11-20 13:08:18 +0800

    登录[30%]，添加维护流程图

--- miniprogram/pages/account/account.js
+++ miniprogram/pages/account/account.js
@@ -9,15 +9,20 @@ Page({
     }
   },
   onLoad: function(a) {
-    this.setData({
-      version: app.globalData.version
-    });
+    this.setData({ version: app.globalData.version });
 
     wx.getSetting({
       success: setting => {
         if (setting.authSetting['scope.userInfo']) {
           wx.getUserInfo({ success: this.settingAccount });
         }
+      }
+    });
+
+    // 监听数据 同步全局
+    Object.defineProperty(this.data, 'userInfo', {
+      set: data => {
+        app.globalData.userInfo = data;
       }
     });
   },
@@ -39,38 +44,47 @@ Page({
       }
     });
   },
-  settingAccount: function (res) {
+  settingAccount: function(res) {
     // 兼容事件处理
     res.detail && (res = res.detail);
 
     // 检测授权
     if (res.userInfo) {
 
+      // 如果本地已存储数据且没过期则用本地的
+      let storage = wx.getStorageSync('userInfo');
+      if (storage && storage.endTime > new Date() / 1000) return this.setData({ userInfo: storage });
+
       // 获取openid
       wx.cloud.callFunction({
         name: 'getOpenId',
         complete: data => {
           res.userInfo.id = data.result;
-          this.setData({ userInfo: res.userInfo });
 
+          /////bate openid
+          res.userInfo.id.openId = "test";
           // 拉取主系统数据
-          wx.request({
-            url: `http://${app.globalData.ip}/api/getUserDetail/${data.result.openId}`,
-            success: res => {
-              if (!res.data.error) { //user not exist
-                
-              } else {
-                /*this.setData({
-                  toast: {
-                    text: "检测到您还未绑定主账号!即将跳转...",
-                    icon: "loading",
-                    hideTime: 3000,
-                    callback: () => {
-                      wx.navigateTo({ url: '../login/login' });
-                    }
-                  }
-                });*/
-              }
+          let getLoginData = this.login(data.result.openId, login => {
+
+            res.userInfo.login = login;
+
+            // 用户是否注册
+            if (login) {
+
+              // 获取账号数据
+              this.getAccountData(res.userInfo, info => {
+                res.userInfo.info = info;
+                res.userInfo.endTime = res.userInfo.login.token.split('-')[2] || (new Date()).valueOf() + 259200;
+                this.setData({ userInfo: res.userInfo });
+                wx.setStorage({
+                  key: 'userInfo',
+                  data: res.userInfo,
+                });
+              });
+            } else {
+
+              // 注册账号
+              this.register(data.result.openId, getLoginData);
             }
           });
 
@@ -85,5 +99,41 @@ Page({
         }
       });
     }
+  },
+  /**
+   * 登录函数
+   * @openId string 小程序ID
+   * @callback object 回调函数
+   * @return 数据
+   */
+  login: function(openId, callback) {
+    wx.request({
+      url: `http://${app.globalData.ip}/api/login/${openId}`,
+      success: res => callback && !res.data.error ? callback(res.data) : callback(false)
+    });
+  },
+  /**
+   * 注册函数
+   * @openId string 小程序ID
+   * @callback object 回调函数
+   * @return 主系统ID
+   */
+  register: function(openId, callback) {
+    wx.request({
+      url: `http://${app.globalData.ip}/api/register/${openId}`,
+      success: res => callback && !res.data.error ? callback(res.data) : callback(false)
+    });
+  },
+  /**
+   * 注册函数
+   * @openId string 小程序ID
+   * @callback object 回调函数
+   * @return 主系统ID
+   */
+  getAccountData: function (userInfo, callback) {
+    wx.request({
+      url: `http://${app.globalData.ip}/api/getUserDetail/${userInfo.login.xcxid}/?token=${userInfo.login.token}`,
+      success: res => callback && !res.data.error ? callback(res.data) : callback(false)
+    });
   }
 });
