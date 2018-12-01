f0d4c6e63db91badf3b381f08ddd5fc8fdfc076d
ShiLaiMu <478889187@qq.com>
2018-12-01 13:46:31 +0800

    图片大小、噪点优化

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -34,8 +34,8 @@ Page({
     // 条件判断
     if (!val.csrq || !(/^(\d){4}-(\d){1,2}-(\d){1,2}$/g).test(val.phone))
       error = "出生日期 格式错误: 1999-01-01!"
-    if (!val.sex || !(/[男女]/g).test(val.phone))
-      error = "性别 格式错误: 男/女";
+    // if (!val.sex || !(/[男女]/g).test(val.phone))
+    //   error = "性别 格式错误: 男/女";
     if (!val.phone || !(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/g))
       error = "姓名 格式错误!";
     if (!val.phone || !(/^[\u4E00-\u9FA5\uf900-\ufa2d]{2,4}$/g).test(val.phone))
