d148c06a019a6c757782424fcf5460dd4a3ac7af
ShiLaiMu <478889187@qq.com>
2018-12-05 21:54:04 +0800

    身份证上传 60%

--- miniprogram/pages/upload/idCard/idcard.js
+++ miniprogram/pages/upload/idCard/idcard.js
@@ -1,19 +1,33 @@
-// miniprogram/pages/upload/idCard/idcard.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isId: "",
+    theId: ""
   },
   
-  camera: function() {
-
+  camera: function(e) {
+    let self = this;
+    console.log(e)
     wx.chooseImage({
       count: 1,
       success: function (res) {
-        console.log(res)
+        self.setData({
+          [e.currentTarget.dataset.name]: res.tempFilePaths[0]
+        });
+        console.log(app.globalData)
+        wx.uploadFile({
+          url: app.globalData.ip + 'api/SetIDCard?token=' + app.globalData.userInfo.token,
+          filePath: res.tempFilePaths[0],
+          name: 'idcard',
+          success: function (res) {
+            console.log(res)
+          },
+          fail: console.log
+        });
       }
     });
   }
