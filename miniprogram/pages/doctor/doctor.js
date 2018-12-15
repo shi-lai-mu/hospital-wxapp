24cdd4f3cc6259f5a37cc649a90ca5e27b091635
ShiLaiMu <478889187@qq.com>
2018-12-15 23:01:42 +0800

    更新界面

--- miniprogram/pages/doctor/doctor.js
+++ miniprogram/pages/doctor/doctor.js
@@ -1,9 +1,23 @@
 const app = getApp();
 Page({
   data: {
-
+    // 菜单标签
+    tag: {},
   },
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    
+    // 默认选中 咨询
+    this.showContent('consulting');
   },
+
+  /**
+   * 菜单点击后显示
+   */
+  showContent: function(e) {
+    this.setData({
+      tag: {
+        [e.target ? e.target.dataset.tag : e]: true
+      }
+    });
+  }
 })
