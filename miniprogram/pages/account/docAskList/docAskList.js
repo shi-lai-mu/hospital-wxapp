467635300a527b87fab4cab225cda27222f95150
shi_lai_mu <478889187@qq.com>
2018-12-22 16:22:27 +0800

    排序消息權重 排序 已回复 -> 待回复 -> 已结束

--- miniprogram/pages/account/docAskList/docAskList.js
+++ miniprogram/pages/account/docAskList/docAskList.js
@@ -2,16 +2,31 @@ const app = getApp();
 Page({
   data: {},
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     app.bar({
       title: "我的咨询",
       bgColor: "#B5CFFF"
     });
 
     app.request(app.globalData.userInfo.token, "getDocAskListByPat", res => {
-      this.setData({
-        docList: res.data
-      });
+      if (res.data) {
+
+        //排序 已回复 -> 待回复 -> 已结束
+        let sorting = [[], [], []];
+        res.data.forEach(value => {
+          if(value.status == 1) {
+            sorting[2].push(value);
+          } else if (value.status == -1) {
+            sorting[1].push(value);
+          } else sorting[0].push(value);
+        });
+        let newArr = []
+        newArr.push(...sorting[0], ...sorting[1], ...sorting[2]);
+
+        this.setData({
+          docList: newArr
+        });
+      }
     });
   },
 
