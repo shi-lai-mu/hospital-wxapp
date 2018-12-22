10d90e7f139765ab3a8e56adc013689a3af11a0e
shi_lai_mu <478889187@qq.com>
2018-12-22 21:38:35 +0800

    添加时间函数的注释和案例

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -244,6 +244,15 @@ App({
 
   /**
    * 返回格式的时间
+   * 对Date的扩展，将 Date 转化为指定格式的String
+   * 月(M)、日(d)、12小时(h)、24小时(H)、分(m)、秒(s)、周(E)、季度(q) 可以用 1-2 个占位符
+   * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+   * eg:
+   * "yyyy-MM-dd hh:mm:ss.S"   ==> 2006-07-02 08:09:04.423
+   * "yyyy-MM-dd E HH:mm:ss"   ==> 2009-03-10 二 20:09:04
+   * "yyyy-MM-dd EE hh:mm:ss"  ==> 2009-03-10 周二 08:09:04
+   * "yyyy-MM-dd EEE hh:mm:ss" ==> 2009-03-10 星期二 08:09:04
+   * "yyyy-M-d h:m:s.S"        ==> 2006-7-2 8:9:4.18
    */
   getDate: function(fmt) {
     let date = new Date();
