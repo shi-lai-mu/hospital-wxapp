fdbcbc19f96f70a9cb6152d728ae70e027156dcf
shi_lai_mu <478889187@qq.com>
2019-01-04 16:25:18 +0800

    跟随api修改目录结构

--- miniprogram/pages/account/askList/askList.js
+++ miniprogram/pages/account/askList/askList.js
@@ -1,18 +1,24 @@
 const app = getApp();
 Page({
-  onShow: function() {
+  onLoad: function(e) {
+
     app.bar({
       title: "我的咨询",
       bgColor: "#B5CFFF"
     });
-
-    app.request(app.globalData.userInfo.token, "getDocAskListByPat", res => {
+    e.type = 'DocClose'
+    // 按页面参数 进行请求
+    e.type && app.request(app.globalData.userInfo.token, `getDocAskListBy${e.type}`, res => {
       if (res.data) {
 
-        //排序 已回复 -> 待回复 -> 已结束
-        let sorting = [[], [], []];
+        //排序 已回复 > 待回复 > 已结束
+        let sorting = [
+          [],// 已回复
+          [],// 待回复
+          [] // 已结束
+        ];
         res.data.forEach(value => {
-          if(value.status == 1) {
+          if (value.status == 1) {
             sorting[2].push(value);
           } else if (value.status == -1) {
             sorting[1].push(value);
@@ -26,6 +32,7 @@ Page({
         });
       }
     });
+
   },
 
 })
